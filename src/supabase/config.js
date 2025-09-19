49c7ea7f4ffa9974dec53ab03b80d08e0c75518b
JackSparr0w999 <giovannipaolotricarico@gmail.com>
2025-09-19 17:11:55 +0200

    modifiche con supabase

--- src/supabase/config.js
+++ src/supabase/config.js
@@ -1,5 +1,5 @@
 
 import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://ruqzgmqcbaqezsepfqdw.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ1cXpnbXFjYmFxZXpzZXBmcWR3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgyOTAzMjAsImV4cCI6MjA3Mzg2NjMyMH0.00G3EQwmqNXUzWGUOv_zP9OPIUlJdAye0Nl1XmtgZg4'
 export const supabase = createClient(supabaseUrl, supabaseKey)
